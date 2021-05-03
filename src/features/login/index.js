39dc3b01e5724cc859690dd2d77b1fedb03d92d8
Mauro Suarez <msuarez@MacBook-Pro-de-Administrador.local>
2021-05-03 15:20:05 -0300

    Add count total words to description field

--- src/features/login/index.js
+++ src/features/login/index.js
@@ -6,8 +6,8 @@ import { initialData } from '../../api/dataService';
 
 const Login = () => {
   const { setToken } = useContext(AppContext);
-  const [username, setUsername] = useState('admin');
-  const [password, setPassword] = useState('admin');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
   const [loading, setLoading] = useState(false);
 
